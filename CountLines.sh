9ed6bba602f1aac384dd9f310edad1ad0faac4a0
Milad Mobini <miladmobini.ca@gmail.com>
2021-02-25 17:27:01 -0500

    new format added

--- CountLines.sh
+++ CountLines.sh
@@ -1,5 +1,5 @@
 #Author: Milad Mobini February 2021
-#This bash script will go recursively through all the [ php, css, html, javascript, java, fxml, arduino, sql, bash]
+#This bash script will go recursively through all the [ php, css, html, javascript, java,python, fxml, arduino, sql, bash]
 #and will count all the lines and add them together
 #This script will avoid all files with name of [*min.css, *all.css, *min.js, *all.js]
 
@@ -12,6 +12,7 @@ find . -name '*.css' -not -path "*min.css" -not -path "*all.css" | xargs wc -l |
 find . -name '*.html' | xargs wc -l | grep total | cut -d 't' -f1 >> result.txt;
 find . -name '*.js'  -not -path "*min.js" -not -path "*all.js" | xargs wc -l | grep total | cut -d 't' -f1 >> result.txt;
 find . -name '*.java' | xargs wc -l | grep total | cut -d 't' -f1 >> result.txt;
+find . -name '*.py' | xargs wc -l | grep total | cut -d 't' -f1 >> result.txt;
 find . -name '*.fxml' | xargs wc -l | grep total | cut -d 't' -f1 >> result.txt;
 find . -name '*.ino' | xargs wc -l | grep total | cut -d 't' -f1 >> result.txt;
 find . -name '*.sql' | xargs wc -l | grep total | cut -d 't' -f1 >> result.txt;
